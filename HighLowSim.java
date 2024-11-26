837fe7c0e0aa60d03d802326ff3bc5cc118a8edd
iamstumped <154484315+iamstumped@users.noreply.github.com>
2024-11-26 17:40:43 -0500

    Add files via upload
    Improved user experience: No longer exits the program upon invalid input. Prompts to run again on completion rather than exit.

--- HighLowSim.java
+++ HighLowSim.java
@@ -27,19 +27,20 @@ public class HighLowSim {
     
     public static void main(String[] args) {
         //constants
-        int SIM_COUNT = 20000;
+        int SIM_COUNT = 50000;
         boolean SHOW_RESULTS = false;
         //variables
         int highCount, lowCount, drawCount;
-        highCount = lowCount = drawCount = 0;
         int input = 0;
+        char repeat = 'y';
+        boolean valid;
         
+        //objects
         Scanner keyboard = new Scanner(System.in);
         
-        Card[] tistaHand = new Card[3];
-        Card[] playerHand = new Card[3];
-        
-        ArrayList<Card> deck = new ArrayList<>(9);
+        Card[] tistaHand;
+        Card[] playerHand;
+        ArrayList<Card> deck;
         
         //command line arguments
         if (args.length > 0) {
@@ -51,104 +52,136 @@ public class HighLowSim {
             }
         }
         
-        //setup
-        for (int i = 0; i < 9; i++) {
-            deck.add(i, new Card(i + 1));
-        }
-        //set first given card
-        System.out.print("Enter Tista's first card: ");
-        try {
-            input = keyboard.nextInt();
-        } 
-        catch (InputMismatchException ime) {
-            System.out.println("\u001B[31mError: Invalid input. Enter an integer between 1 and 9 (inclusive).\u001B[0m");
-            pause();
-            System.exit(0);
-        }
-        keyboard.nextLine();
-        if (deck.remove(new Card(input))) {
-            tistaHand[0] = new Card(input);
-        } else {
-            System.out.println("\u001B[31mError: Invalid number. Enter an integer between 1 and 9 (inclusive) and do not repeat.\u001B[0m");
-            pause();
-            System.exit(0);
-        }
-        //set second given card
-        System.out.print("Enter Tista's second card: ");
-        try {
-            input = keyboard.nextInt();
-        } 
-        catch (InputMismatchException ime) {
-            System.out.println("\u001B[31mError: Invalid input. Enter an integer between 1 and 9 (inclusive).\u001B[0m");
-            pause();
-            System.exit(0);
-        }
-        keyboard.nextLine();
-        if (deck.remove(new Card(input))) {
-            tistaHand[1] = new Card(input);
-        } else {
-            System.out.println("\u001B[31mError: Invalid number. Enter an integer between 1 and 9 (inclusive) and do not repeat.\u001B[0m");
-            pause();
-            System.exit(0);
-        }
-        //set third given card
-        System.out.print("Enter your card: ");
-        try {
-            input = keyboard.nextInt();
-        } 
-        catch (InputMismatchException ime) {
-            System.out.println("\u001B[31mError: Invalid input. Enter an integer between 1 and 9 (inclusive).\u001B[0m");
-            pause();
-            System.exit(0);
-        }
-        keyboard.nextLine();
-        if (deck.remove(new Card(input))) {
-            playerHand[0] = new Card(input);
-        } else {
-            System.out.println("\u001B[31mError: Invalid number. Enter an integer between 1 and 9 (inclusive) and do not repeat.\u001B[0m");
-            pause();
-            System.exit(0);
-        }
-        
-        //simulations
-        long startTime = System.nanoTime();
-        for (int i = 0; i < SIM_COUNT; i++) {
-            //randomly deal face-down cards
-            shuffle(deck); 
+        while (repeat == 'y') {
+            //resetting
+            highCount = lowCount = drawCount = 0;
             
-            tistaHand[2] = deck.get(0);
-            playerHand[1] = deck.get(1);
-            playerHand[2] = deck.get(2);
+            tistaHand = new Card[3];
+            playerHand = new Card[3];
+            deck = new ArrayList<>(9);
             
-            //record results
-            if (sumHand(playerHand) > sumHand(tistaHand)) {
-                if (SHOW_RESULTS) System.out.println("HIGH");
-                highCount++;
-            }
-            else if (sumHand(playerHand) < sumHand(tistaHand)) {
-                if (SHOW_RESULTS) System.out.println("LOW");
-                lowCount++;
-            }
-            else {
-                if (SHOW_RESULTS) System.out.println("DRAW");
-                drawCount++;
+            //setup
+            for (int i = 0; i < 9; i++) {
+                deck.add(i, new Card(i + 1));
             }
             
+            //set first given card
+            valid = false;
+            do {
+                System.out.print("Enter Tista's first card: ");
+                //try catch to ensure integer input
+                try {
+                    //get input
+                    input = keyboard.nextInt();
+                    //check if the card is in the deck
+                    if (deck.remove(new Card(input))) {
+                        //if it is, continue
+                        tistaHand[0] = new Card(input);
+                        valid = true;
+                    } else {
+                        //if not, print error and retry
+                        System.out.println("\u001B[31mError: Invalid number. Enter an integer between 1 and 9 (inclusive) and do not repeat.\u001B[0m");
+                    }
+                } 
+                catch (InputMismatchException ime) {
+                    System.out.println("\u001B[31mError: Invalid input. Enter an integer between 1 and 9 (inclusive).\u001B[0m");
+                }
+                keyboard.nextLine();  
+            } while (!valid);
+            
+            
+            //set second given card
+            valid = false;
+            do {
+                System.out.print("Enter Tista's second card: ");
+                //try catch to ensure integer input
+                try {
+                    //get input
+                    input = keyboard.nextInt();
+                    //check if the card is in the deck
+                    if (deck.remove(new Card(input))) {
+                        //if it is, continue
+                        tistaHand[1] = new Card(input);
+                        valid = true;
+                    } else {
+                        //if not, print error and retry
+                        System.out.println("\u001B[31mError: Invalid number. Enter an integer between 1 and 9 (inclusive) and do not repeat.\u001B[0m");
+                    }
+                } 
+                catch (InputMismatchException ime) {
+                    System.out.println("\u001B[31mError: Invalid input. Enter an integer between 1 and 9 (inclusive).\u001B[0m");
+                }
+                keyboard.nextLine();  
+            } while (!valid);
+            
+            
+            //set third given card
+            valid = false;
+            do {
+                System.out.print("Enter your card: ");
+                //try catch to ensure integer input
+                try {
+                    //get input
+                    input = keyboard.nextInt();
+                    //check if the card is in the deck
+                    if (deck.remove(new Card(input))) {
+                        //if it is, continue
+                        playerHand[0] = new Card(input);
+                        valid = true;
+                    } else {
+                        //if not, print error and retry
+                        System.out.println("\u001B[31mError: Invalid number. Enter an integer between 1 and 9 (inclusive) and do not repeat.\u001B[0m");
+                    }
+                } 
+                catch (InputMismatchException ime) {
+                    System.out.println("\u001B[31mError: Invalid input. Enter an integer between 1 and 9 (inclusive).\u001B[0m");
+                }
+                keyboard.nextLine();  
+            } while (!valid);
+
+            //simulations
+            long startTime = System.nanoTime();
+            for (int i = 0; i < SIM_COUNT; i++) {
+                //randomly deal face-down cards
+                shuffle(deck); 
+
+                tistaHand[2] = deck.get(0);
+                playerHand[1] = deck.get(1);
+                playerHand[2] = deck.get(2);
+
+                //record results
+                if (sumHand(playerHand) > sumHand(tistaHand)) {
+                    if (SHOW_RESULTS) System.out.println("HIGH");
+                    highCount++;
+                }
+                else if (sumHand(playerHand) < sumHand(tistaHand)) {
+                    if (SHOW_RESULTS) System.out.println("LOW");
+                    lowCount++;
+                }
+                else {
+                    if (SHOW_RESULTS) System.out.println("DRAW");
+                    drawCount++;
+                }
+
+            }
+            long endTime = System.nanoTime();
+
+            //show results
+            System.out.println("\n--COMPLETE--\nSimulations run: " + SIM_COUNT + "\n" + "Elapsed time: " + String.format("%.03f", (endTime - startTime)/1000000.0) + " ms\n");
+
+            if (highCount > lowCount) {System.out.print("\u001B[32m");}
+            System.out.println("High: " + highCount + " time" + (highCount != 1 ? "s, " : ", ") + String.format("%7.03f", (highCount/(float)SIM_COUNT)*100) + "% chance\u001B[0m");
+
+            if (lowCount > highCount) {System.out.print("\u001B[32m");}
+            System.out.println("Low:  " + lowCount + " time" + (lowCount != 1 ? "s, " : ", ") + String.format("%7.03f", (lowCount/(float)SIM_COUNT)*100) + "% chance\u001B[0m");
+
+            System.out.println("Draw: " + drawCount + " time" + (drawCount != 1 ? "s, " : ", ") + String.format("%7.03f", (drawCount/(float)SIM_COUNT)*100) + "% chance\n");
+
+            System.out.print("Run again? (y/n): ");
+            repeat = keyboard.nextLine().trim().charAt(0);
+            
+            System.out.println("------------------\n\n");
         }
-        long endTime = System.nanoTime();
-        
-        //show results
-        System.out.println("\n--COMPLETE--\nSimulations run: " + SIM_COUNT + "\n" + "Elapsed time: " + String.format("%.03f", (endTime - startTime)/1000000.0) + " ms\n");
-        
-        if (highCount > lowCount) {System.out.print("\u001B[32m");}
-        System.out.println("High: " + highCount + " time" + (highCount != 1 ? "s, " : ", ") + String.format("%7.03f", (highCount/(float)SIM_COUNT)*100) + "% chance\u001B[0m");
-        
-        if (lowCount > highCount) {System.out.print("\u001B[32m");}
-        System.out.println("Low:  " + lowCount + " time" + (lowCount != 1 ? "s, " : ", ") + String.format("%7.03f", (lowCount/(float)SIM_COUNT)*100) + "% chance\u001B[0m");
-        
-        System.out.println("Draw: " + drawCount + " time" + (drawCount != 1 ? "s, " : ", ") + String.format("%7.03f", (drawCount/(float)SIM_COUNT)*100) + "% chance\n");
-        
-        pause();
     }
     
 }
